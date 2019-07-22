b6147ae20bd20a89f444b3a587529fd84f7b9202
seaiiok <seaii@qq.com>
2019-07-22 21:26:40 +0800

    comments

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "remote push code..."
+
+File_PATH=D:\\GO\\src\\A
+
+cd $File_PATH
+
+git add -A
+echo -n "enter commit comments:"
+read comments
+# if comments=="" then
+# comments="administrator"
+# fi
+git commit -m comments
+git push origin master
+sleep 1
+
