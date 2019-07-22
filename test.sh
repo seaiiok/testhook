a4dff4d412a11ebf3354716fa5380509eb7c650d
seaiiok <seaii@qq.com>
2019-07-22 21:44:17 +0800

    administrator

--- test.sh
+++ test.sh
@@ -9,10 +9,13 @@ cd $File_PATH
 git add -A
 echo -n "enter commit comments:"
 read comments
-# if comments=="" then
-# comments="administrator"
-# fi
+if ["$comments" -eq ""] 
+then
+comments="administrator"
+fi
+echo $comments
 git commit -m $comments
 git push origin master
-sleep 1
+sleep 10
+
 
